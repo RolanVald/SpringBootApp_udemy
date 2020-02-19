7667ff3f980d0b93bcdc246abe84ccd0d0958fef
RolanVald <roolii95@gmail.com>
2020-02-19 00:12:52 -0600

    Add create client: back - front

--- spring-boot-data-jpa/src/main/java/com/rolval/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/rolval/springboot/app/controllers/ClienteController.java
@@ -1,12 +1,16 @@
 package com.rolval.springboot.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.rolval.springboot.app.models.dao.IClienteDao;
+import com.rolval.springboot.app.models.entity.Cliente;
 
 @Controller
 public class ClienteController {
@@ -21,5 +25,19 @@ public class ClienteController {
 		model.addAttribute("titulo", "listado de clientes");
 		model.addAttribute("Clientes", clienteDAO.findAll());
 		return "listar";
+	}
+	
+	@GetMapping("/form")
+	public String crear(Map<String, Object> model) {
+		Cliente cliente = new Cliente();
+		model.put("cliente", cliente);
+		model.put("titulo", "Formulario de cliente");
+		return "form";
+	}
+	
+	@PostMapping("/form")
+	public String guardar(Cliente cliente) {
+		clienteDAO.save(cliente);
+		return "redirect:listar";
 	}
 }
